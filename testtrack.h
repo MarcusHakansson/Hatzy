17c9ac849b3890f11cd09f47d268abedf5464b19
Marcus <marcus.hakansson@parkarna.se>
2016-09-26 14:47:00 +0200

    Upload first files

---
+++ testtrack.h
@@ -0,0 +1,14 @@
+// Arduino Flappy Bird homage by augustzf@gmail.com
+
+byte kDigits[][8] = {
+{
+  B00000000,
+  B00000000,
+  B00000000,
+  B00000000,
+  B00000000,
+  B00000000,
+  B00000000,
+  B00000000 
+}
+};
